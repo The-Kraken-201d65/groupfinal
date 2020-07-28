3a0797ab0e5a580656a8581fbd5d4d6856be1c15
Darius Lee Pasilaban <p.dariuslee@gmail.com>
2020-07-27 17:42:11 -0700

    created constructor Location

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
-'user strict'
+'user strict';
+
+Location.locationsArray = [];
+
+var Location = function(name, src) {
+  this.name = name;
+  this.src = src;
+  this.hearted = false;
+  this.thumbDown = false;
+  Location.locationsArray.push(this);
+}
+
 
 
 
